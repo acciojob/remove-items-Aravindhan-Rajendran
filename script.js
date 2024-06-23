ca30b9b4467e55147051732e17444578cd0b8c22
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-06-23 21:59:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let element=document.getElementById("button");
+element.addEventListener("click",removing);
+let color=document.getElementById("colorSelect");
+let onee=document.getElementById("one");
+let twoo=document.getElementById("two");
+let three=document.getElementById("three");
+let fourr=document.getElementById("four");
+function removing(){
+color.remove(color.selectedIndex);
+	
+}
